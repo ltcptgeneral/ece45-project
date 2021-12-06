9be6ba39916fe519bef874c5d8c73f4edf4e9d47
khannuuuuur <hsu.conner@gmail.com>
2021-12-05 22:17:23 -0800

    fixing

--- src/amplify.m
+++ src/amplify.m
@@ -1,5 +1,8 @@
 function output = amplify(input, multiplier)
-    %Amplifies the signal by multiplier
-    %By Conner Hsu
+    %input: a 1D array representing the sound signal in the time domain
+    %multiplier: a scalar that multiplier all values in the input array to
+    %            amplify or decrease the volume.
+    %Returns: input signal scaled by the multiplier in the time domain.
+    %Author: Conner Hsu
     output = input*multiplier;
 end

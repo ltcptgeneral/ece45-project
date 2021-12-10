52a3bee8440a4a864871fcd2069c1188c96fab22
k7xu <k7xu@ucsd.edu>
2021-12-09 21:32:24 -0800

    Fixed my generator

--- src/Generators/generate_pulse.m
+++ src/Generators/generate_pulse.m
@@ -13,14 +13,21 @@ function x = generate_pulse(amplitude, frequency, phase, fs, duration, duty)
     % initialize local variables from input arguments
     n = fs * duration; % number of samples (length of matrix)
     dt = 1 / fs; % sampling period: time between two sample points
-    
     % initialize a one dimensional zero matrix to be populated
     x = zeros(1, n);
-    
+    f0=1e+6;  % 1MHz
+    Fs=3e+6;
+    Tf=0.001;  % 1 millisecond
+    t=0:1/Fs:Tf-1/Fs;
+    td=0.1;   % duty cycle 
+    A0=10; % 10 Volts
+    F=0;
+    N=1000; % Number of points 
+
     % populate the matrix
-    for i = 1:n
-       F=F+(1/n)*cos(n*2*pi*(1e+6)*t).*sin(n*pi*0.1);
+    for n = 1:N
+        F=F+(1/n)*cos(n*2*pi*f0*t).*sin(n*pi*td);
     end
-    F=F*(2*10/pi);
-    F=F+10*0.1;
+    F=F*(2*A0/pi);
+    F=F+A0*td;
 end

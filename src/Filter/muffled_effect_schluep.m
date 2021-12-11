6dc3ce4d59fd929d933953b299f55c7fcdd24465
Nicolas Schluep <nschluep@ucsd.edu>
2021-12-10 22:10:26 -0800

    Fixed Functions Nicolas Schluep

--- src/Filter/muffled_effect_schluep.m
+++ src/Filter/muffled_effect_schluep.m
@@ -13,13 +13,22 @@ function output = muffled_effect_schluep(input, Fs, LOW, MED, HIGH)
 % HIGH: The maximum frequency that the low-pass filter will let pass. A
 % typical value for this variable is 1000 Hz.
 
-non_stereophonic = input(:, 1);     % Removes the sterophonic property of the input sound
-% by just taking the first column of data.
+n = size(input, 2);
 
-Len = length(non_stereophonic);
+non_stereophonic = input;
+
+if (n == 1) || (n == 2) 
+    non_stereophonic = input(:, 1);     % Removes the sterophonic property of the input sound
+    % by just taking the first column of data.
+    non_stereophonic = transpose(non_stereophonic); 
+end
+
+modified_input = non_stereophonic(1, :);
+
+Len = length(modified_input);
 F = Fs * ((-Len/2) : ((Len/2) - 1)) / Len;  % Creating the array of frequencies
 % which the FFT Shifted version of the signal can be plotted against.
-inputFreq = fftshift(fft(non_stereophonic));   % Creates the Fourier Transform of the
+inputFreq = fftshift(fft(modified_input));   % Creates the Fourier Transform of the
 % input signal. fftshift() makes it such that the zero frequency is at the 
 % center of the array.
 lowPassFilter = zeros(1, length(inputFreq));
@@ -33,7 +42,8 @@ for i = 1:length(lowPassFilter)
     end
 end
 
-lowPassedInput = inputFreq .* transpose(lowPassFilter); %Apply the low-pass filter.
+lowPassedInput = inputFreq .* lowPassFilter; %Apply the low-pass filter.
+lowPassedInput = transpose(lowPassedInput);
 
 % Adding a slight reverb effect.
 realOutput = real(ifft(fftshift(lowPassedInput)));
@@ -49,6 +59,10 @@ delayedOutput = delayedOutput(1:length(realOutput));    % Truncates the "delayed
 output = (realOutput + delayedOutput) ./ 2.0;    % Adds the "realOutput" and "delayedOutput"
 % vectors to create a reverb effect. Divides by 2 to avoid clipping
 % effects.
+
+output = transpose(output);
+
+end
 
 
 
